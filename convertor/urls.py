b13d057bb223b30fa29ac601e05ad3d7ad8fd26a
hosein <hoseinlaki16@gmail.com>
2024-07-25 12:44:05 +0330

    fix view name in urls

--- convertor/urls.py
+++ convertor/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'convertor'
 
 urlpatterns = [
-    path('image-to-pdf/', views.ImageToPDFView.as_view(), name='image_to_pdf'),
+    path('image-to-pdf/', views.ImageToPDFApiView.as_view(), name='image_to_pdf'),
 ]
